ee1f62647771cb870061c225914fde5358e227a6
Nelsonsibiya05 <nelsonsibiya05@gmail.com>
2023-02-16 13:19:45 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts funtion
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is lile building a multilingual puzzle");
+	return (0);
+}
