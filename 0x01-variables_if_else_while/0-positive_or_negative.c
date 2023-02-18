147337bfab8bd1a3d9200f468f5740f1c0e8efa0
Nelsonsibiya05 <nelsonsibiya05@gmail.com>
2023-02-17 18:13:55 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,18 +6,18 @@
  * main - This program uses the function r and() to output
  * random number to the screen, then tell s the user if it's
  * positive, negative, or a zero.
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX /2;
-	if (n>0)
-		printf("%d is positive\n" , n);
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("%d is negative\n" , n);
+		printf("%d is negative\n", n);
 
-	return (0)
+	return (0);
 }
