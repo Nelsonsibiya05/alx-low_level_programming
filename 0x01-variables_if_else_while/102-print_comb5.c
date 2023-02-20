d59b093de66e4822b09e23773b4df2c25a1a2b08
Nelsonsibiya05 <nelsonsibiya05@gmail.com>
2023-02-20 15:43:20 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * maim - The program prints the number from 0 to 99
+ * Number are separated by a comma anda space, are printed
+ * in ascending order, and putchar is used to screen.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (b = '0'; b <= '9'; b++)
+	{
+		for (a = '0'; a <= '9'; a++)
+		{
+			putchar(b);
+			putchar(a);
+			if (b != '9' || a != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
