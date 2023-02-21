9eb91bb4e9949395977fa8dc5b609fd0b2785a76
Nelsonsibiya05 <nelsonsibiya05@gmail.com>
2023-02-20 18:33:51 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The purpose of this file is to output
+ * the string.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+
+	return (0);
+}
