ec5a8fb5135d0ca53778e95e563d9a4d3309a608
Nelsonsibiya05 <nelsonsibiya05@gmail.com>
2023-02-22 03:43:36 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_xl0.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times the alphabet, in lowcase
+ * followed by new line
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char la;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la);
+		_putchar('\n';
+	}
+}
