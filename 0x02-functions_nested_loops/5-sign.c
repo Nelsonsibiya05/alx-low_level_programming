4e2751960f920f92a46e533586047b00fbb57405
Nelsonsibiya05 <nelsonsibiya05@gmail.com>
2023-02-22 15:33:35 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sigh - prints the sigh of a number
+ * @n: the numbe of the sign to the printed
+ * Return: 1 if  number is greater than zere
+ * 0 of number is zero
+ * -1 if number is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n = 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (0);
+	}
+}
