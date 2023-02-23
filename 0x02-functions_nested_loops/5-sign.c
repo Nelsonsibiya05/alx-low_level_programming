2af923a680a012c4c6fef6afea6838c6cfb2ecc4
Nelsonsibiya05 <nelsonsibiya05@gmail.com>
2023-02-22 16:27:25 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,7 +14,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n = 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
@@ -22,6 +22,6 @@ int print_sign(int n)
 	else
 	{
 		_putchar('-');
-		return (0);
+		return (-1);
 	}
 }
