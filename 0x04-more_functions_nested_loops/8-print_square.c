8c394ecacbdbeffb2cc956927199a559204326f6
Nelsonsibiya05 <nelsonsibiya05@gmail.com>
2023-02-26 13:38:43 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "maim.h"
+
+/**
+ * print_square - This function prints aquare
+ * followed by a new line
+ * @size: The size of the square
+ */
+void print_square(int size)
+{
+	int row, column;
+
+	if (size > 0)
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (column = 0; row < size; column++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n);
+		}
+	}
+	else
+		_putchar('\n);
+
+}
