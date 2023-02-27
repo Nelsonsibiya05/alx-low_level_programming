fcace4108880fbe923293813c4c243bb0e6d77bf
Nelsonsibiya05 <nelsonsibiya05@gmail.com>
2023-02-27 11:33:46 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - This function prints one char out of 2 of a string
+ * followed by a new line
+ * @str: The string to be evaluated
+ */
+void puts2(char *str)
+{
+	int i;
+	chat ch;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		if (i % 2 == 0)
+		{
+			ch = str[i];
+			_putchar(ch);
+		}
+	}
+
+	_putchar('\n');
+}
