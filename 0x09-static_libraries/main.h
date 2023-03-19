bee8e2b161c2d11643b0074b81edb1d7d1ecb6bd
Nelsonsibiya05 <nelsonsibiya05@gmail.com>
2023-03-19 03:46:41 -0700

    main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * file: main
+ *
+ * Desc: header file
+ */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,4 +28,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endf /* MAIN_H */
+#endif
