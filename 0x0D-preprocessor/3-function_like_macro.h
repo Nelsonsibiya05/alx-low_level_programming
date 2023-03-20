9e3f317bde2906e4e8c413fef44e2c4b53662350
Nelsonsibiya05 <nelsonsibiya05@gmail.com>
2023-03-20 10:07:03 -0700

    file name

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef _3_FUNCTION_LIKE_MACRO_H_
 #define _3_FUNCTOIN_LIKE_MACRO_H_
 
-#define ABS(x) (((x) < 0) ? -(x) : (x))
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
 
 #endif /* _3_FUNCTION_LIKE_MACRO_H_ */
 
