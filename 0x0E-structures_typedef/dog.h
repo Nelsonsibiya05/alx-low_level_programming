03aa91b8bbea290f30ba2920f7c2de24f6f869cf
Nelsonsibiya05 <nelsonsibiya05@gmail.com>
2023-03-21 12:26:43 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,11 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef STRTYP
+#define STRTYP
 
 /**
- * struct dog - This struct creates a new type
- * @name: First member, char poiter
- * @age: Second member, float
- * @owner: Third member, char poiter
- *
- * Description: This struct has three elements and is a new type dog
+ * struct dog - structure of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  */
 
 struct dog
@@ -17,9 +15,6 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog - Typdef for dog type
- */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -27,5 +22,4 @@ void priny_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(doh_t *d);
 
-#endif /* _DOG_H_ */
-
+#endif /*STRTYP*/
