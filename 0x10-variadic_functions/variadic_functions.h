49c4922c9cafd462a7c8216c11ba359c1b6582c6
Nelsonsibiya05 <nelsonsibiya05@gmail.com>
2023-03-29 03:07:22 -0700

    update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,4 @@
-#ifendef _VARIADIC_FUNCTIONS_H_
+#ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
 #include <stdarg.h>
 
@@ -8,4 +8,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsignet int n, ...);
 void print_all(const char * const format, ...);
 
-#endif
+#endif /* _VARIADIC_FUNCTIONS_H_ */
